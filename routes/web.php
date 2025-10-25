6bf01fda62db4ef39a06fea1c84255892a994ece
Joel Xaverl <joxabelajar@gmail.com>
2025-10-25 17:02:20 +0200

    CMS Route Roles

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -20,7 +22,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // permission routes
     Route::resource('/permissions', PermissionController::class);
+
+    // permission routes
+    Route::resource('/roles', RoleController::class)->except('show');
+
+     // profile routes
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
